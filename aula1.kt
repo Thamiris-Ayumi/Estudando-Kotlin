cc4f53a695f42412ad932a88c7b3f23bf7de01ab
Thamiris-Ayumi <thamiris.ishikawa@sptech.school>
2023-02-02 11:14:54 -0300

    Introdução à linguagem Kotlin

---
+++ aula1.kt
@@ -0,0 +1,15 @@
+// ESTUDANDO KOTLIN
+// fun = indica que criamos uma function
+// main = indica que o código terá seu início na execução aqui
+
+fun main(){
+    println("Hello, Bytebanker!")
+
+    // Ao criar uma var devemos definir se ela pode ou não mudar o seu valor
+    // var = pode ser modificado
+    // val = não pode ser modificado
+    var titular = "Thamiris"
+
+    // Executando:
+    println(titular)
+}
