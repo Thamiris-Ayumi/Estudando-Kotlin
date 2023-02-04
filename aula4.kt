f1a495afad051ef0134807ae9939ce25a701c2f7
Thamiris-Ayumi <thamiris.ishikawa@sptech.school>
2023-02-03 23:53:57 -0300

    Utilização de classes e objetos

---
+++ aula4.kt
@@ -0,0 +1,45 @@
+// Orientação a Objetos
+
+
+fun main(){
+    println("Hello, Bytebanker!")
+    val conta = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1001
+    contaAlex.saldo = 0.0
+
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+
+    println("Hello, Bytebanker!")
+    val conta = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1002
+    contaFran.saldo = 0.0
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+}
+
+
+// Para nomear uma class, devemos utilizar a primeira letra maiúscula
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0 
+}
+
+// Laço de repetição
+for (i in 1..5){
+        var titular: String = "Cliente $i"
+        var numeroConta: Int = 1000 + i
+        var saldo = 0.0
+
+        // Executando:
+        println("Titular: $titular")
+        println("Número da conta: $numeroConta")
+        println("Saldo: $saldo")
+        println()
+}
