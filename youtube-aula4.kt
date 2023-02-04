a6b521edfafb75676f6daffae5cf851b16b9b9be
Thamiris-Ayumi <thamiris.ishikawa@sptech.school>
2023-02-04 20:32:31 -0300

    Condicionais e operadores lógicos

---
+++ youtube-aula4.kt
@@ -0,0 +1,26 @@
+// Condicionais
+
+/* Operadores Lógicos
+    E &&
+    Ou || 
+*/
+
+// Estrutura condicional
+    var idadeAtual = 18
+
+    if (idadeAtual <= 10) {
+        println("Criança")
+    } else if (idadeAtual <= 17) {
+        println("Adolescente")
+    } else {
+        println("Adulto")
+    }
+
+
+// Operadores Lógicos
+    if (20 == 20 && 10 < 20){
+        println("Verdadeiro")
+    } else {
+        println("Falso")
+}
+    
