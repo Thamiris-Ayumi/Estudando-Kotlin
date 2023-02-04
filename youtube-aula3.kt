16a1db6079d7ad85e444520feed494b08bcf69ca
Thamiris-Ayumi <thamiris.ishikawa@sptech.school>
2023-02-04 18:30:20 -0300

    Operadores aritméticos e relacionais

---
+++ youtube-aula3.kt
@@ -0,0 +1,30 @@
+// Operações Aritméticas
+
+/* Operadores Aritméticos
+    Soma +
+    Subtração -
+    Divisão /
+    Multiplicação *
+    Resto da divisão %
+    Incremento ++
+    Decremento --
+*/
+
+/* Operadores Relacionais
+    Igual ==
+    Diferente !=
+    Menor <
+    Maior >
+    Menor igual <=
+    Maior ogual >=
+*/
+
+
+fun main(args: Array<String>){
+    // Operadores aritméticos
+    var numero1 = 20
+    var numero2 = 10
+
+    println(numero1 / numero2) // Resultado = 2
+    println(numero1 % numero2) // Resto da divisão será 0
+}
