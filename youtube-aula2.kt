c032756c649b7a0b88377b5fd30e8e74ad0aa192
Thamiris-Ayumi <thamiris.ishikawa@sptech.school>
2023-02-04 18:19:27 -0300

    Concatenação

---
+++ youtube-aula2.kt
@@ -0,0 +1,12 @@
+// Concatenação
+
+// Através da Concatenação é possível formar frases mais dinâmicas:
+    var nomeUser= "Thamiris"
+    var idade = 19
+    var profissao = "Programadora"
+
+    // Concatenação no JAVA
+    println("Meu nome é " + nomeUser + ", minha idade é " + idade + " e eu sou " + profissao)
+
+    // Concatenação no KOTLIN
+    println("Meu nome é $nome, minha idade é $idade e eu sou $profissao")
